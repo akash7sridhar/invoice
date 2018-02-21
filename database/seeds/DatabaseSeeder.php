a74275df312447101f0eb9ea2409d3b81d101421
satheesh@dreamstudio.in <satheesh@dreamhost.in>
2018-02-21 17:37:39 +0530

    add migrate:refresh in db seed command

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        if ($this->command->confirm('Do you wish to refresh migration before seeding, Make sure it will clear all old data ?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->warn("Data deleted, starting from fresh database.");
+        }
     }
 }
